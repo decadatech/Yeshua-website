72a541334b8fc687f316c4d61e031020b25648a6
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-06 21:08:44 -0300

    Initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('#swiperIntro', {
+    pagination: {
+        el: '#paginationIntro',
+        type: 'fraction',
+    },
+});
+
+
+var swiper2 = new Swiper('#swiperMulti', {
+  slidesPerView: 3,      
+});
+
+ var swiper3 = new Swiper('#swiperTeam');
