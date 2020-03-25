042cd84364775d639d5f050766687d6a977bf845
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-25 13:13:38 -0300

    Adding contact info

--- contact.php
+++ contact.php
@@ -51,77 +51,93 @@
   </section>
 
   <section>
+    <h1>Vamos conversar?</h1>
+    <h5>Estamos ansiosos para te atender!</h5>
+
     <form class="container">
-      <h1>Vamos conversar?</h1>
-      <h5>Estamos ansiosos para te atender!</h5>
+      <div class="row">
+        <div class="col-md-8">
 
-      <div class="form-row">
-        <div class="form-group col-md-8">
-          <label for="name">Nome:</label>
-          <input type="text" class="form-control" id="name" required>
+          <div class="form-row">
+            <div class="form-group col-md-8">
+              <label for="name">Nome:</label>
+              <input type="text" class="form-control" id="name" required>
+            </div>
+            <div class="form-group col-md-4">
+              <label for="company">Empresa: (Opcional)</label>
+              <input type="text" class="form-control" id="company">
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-8">
+              <label for="address">Enreço:</label>
+              <input type="text" class="form-control" id="address" required>
+            </div>
+            <div class="form-group col-md-4">
+              <label for="complement">Complemento: (Opcional)</label>
+              <input type="text" class="form-control" id="complement">
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="neighborhood">Bairro:</label>
+              <input type="text" class="form-control" id="neighborhood" required>
+            </div>
+            <div class="form-group col-md-4">
+              <label for="city">Cidade:</label>
+              <input type="text" class="form-control" id="city" required>
+            </div>
+            <div class="form-group col-md-2">
+              <label for="state">UF:</label>
+              <select class="form-control" id="state" required>
+                <option>SP</option>
+                <option>RJ</option>
+                <option>-</option>
+                <option>-</option>
+                <option>-</option>
+              </select>
+            </div>
+            <div class="form-group col-md-2">
+              <label for="zipcode">CEP:</label>
+              <input type="text" class="form-control" id="zipcode" required>
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-8">
+              <label for="email">E-mail:</label>
+              <input type="email" class="form-control" id="email" required>
+            </div>
+            <div class="form-group col-md-4">
+              <label for="phone">Telefone:</label>
+              <input type="text" class="form-control" id="phone" required>
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-12">
+              <label for="message">Mensagem:</label>
+              <textarea class="form-control" id="message" required></textarea>
+            </div>
+          </div>
+
+          <button type="submit" class="">Enviar</button>
+
         </div>
-        <div class="form-group col-md-4">
-          <label for="company">Empresa: (Opcional)</label>
-          <input type="text" class="form-control" id="company">
+
+        <div class="col-md-4 contact-info">
+          <h4>Informações de contato</h4>
+          <p><i class="fas fa-map-marker-alt"></i> Bragança Pta.</p> 
+          <p><i class="fas fa-envelope"></i> salesrepsy@gmail.com</p>
+          <p><i class="fas fa-envelope"></i> julianayeshua@gmail.com</p>
+          <p><i class="fas fa-phone-alt"></i> (11) 97618.8465 (Whatsapp)</p>
+          <p><i class="fas fa-phone-alt"></i> (11) 98992.4474 (Whatsapp)</p>
         </div>
       </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-8">
-          <label for="address">Enreço:</label>
-          <input type="text" class="form-control" id="address" required>
-        </div>
-        <div class="form-group col-md-4">
-          <label for="complement">Complemento: (Opcional)</label>
-          <input type="text" class="form-control" id="complement">
-        </div>
-      </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-4">
-          <label for="neighborhood">Bairro:</label>
-          <input type="text" class="form-control" id="neighborhood" required>
-        </div>
-        <div class="form-group col-md-4">
-          <label for="city">Cidade:</label>
-          <input type="text" class="form-control" id="city" required>
-        </div>
-        <div class="form-group col-md-2">
-          <label for="state">UF:</label>
-          <select class="form-control" id="state" required>
-            <option>SP</option>
-            <option>RJ</option>
-            <option>-</option>
-            <option>-</option>
-            <option>-</option>
-          </select>
-        </div>
-        <div class="form-group col-md-2">
-          <label for="zipcode">CEP:</label>
-          <input type="text" class="form-control" id="zipcode" required>
-        </div>
-      </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-8">
-          <label for="email">E-mail:</label>
-          <input type="email" class="form-control" id="email" required>
-        </div>
-        <div class="form-group col-md-4">
-          <label for="phone">Telefone:</label>
-          <input type="text" class="form-control" id="phone" required>
-        </div>
-      </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-12">
-          <label for="message">Mensagem:</label>
-          <textarea class="form-control" id="message" required></textarea>
-        </div>
-      </div>
-
-      <button type="submit" class="">Enviar</button>
     </form>
+
   </section>
 
   <section class="footer">
@@ -158,13 +174,9 @@
   </section>
 
   <!---JQUERY--->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <!---BS POPPER JS--->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <!---BS JS--->
   <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
   <!-- NAVBAR JS -->
